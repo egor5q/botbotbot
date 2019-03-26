1dee47f9b3a46b7e0c4ea78a9f03228682eb505c
egor5q <31898567+egor5q@users.noreply.github.com>
2019-03-26 23:24:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,9 +113,13 @@ def mutee(m):
         member=bot.get_chat_member(m.chat.id, m.from_user.id)
         if member.status=='administrator' or member.status=='creator':
             text=m.text.split(' ')
-            timee=text[1]
-            i=int(timee[:-1])
-            number=timee[len(timee)-1]
+            try:
+                timee=text[1]
+                i=int(timee[:-1])
+                number=timee[len(timee)-1]
+            except:
+                i=0
+                number='m'
             
             untildate=int(time.time())
             if number=='m':
@@ -132,9 +136,15 @@ def mutee(m):
             
             if m.reply_to_message!=None:
                 bot.restrict_chat_member(can_send_messages=False, user_id=m.reply_to_message.from_user.id, chat_id=m.chat.id, until_date=untildate)
-                bot.send_message(m.chat.id, 'Замутил на '+str(i)+' '+datetext)
+                if i==0:
+                    text='навсегда'
+                else:
+                    text='Замутил на '+str(i)+' '+datetext+'.'
+                bot.send_message(m.chat.id, text)
+        else:
+            bot.send_message(m.chat.id, 'Ты сука кто такой чтобы это делать!?')
       except Exception as e:
-        bot.send_message(441399484, traceback.format_exc())
+        bot.send_message(m.chat.id, 'Ошибка мута.')
 
             
 
